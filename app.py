d4d051032720659d0330b55261866873d4ca0f36
Alex Collino <arcollino@gmail.com>
2022-06-14 13:48:36 -0700

    implement routes to questions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template
 from surveys import satisfaction_survey
 
 app = Flask(__name__)
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.route("/")
 def home():
   return render_template("base.html", survey=satisfaction_survey)
+
+@app.route("/questions/<number>")
+def question(number):
+  index = int(number)
+  if index < len(satisfaction_survey.questions):
+    question = satisfaction_survey.questions[index]
+    question_info = {"number": index, "question": question, "next": str(index+1)}
+    return render_template("question.html", q_dict = question_info)
+  else:
+    return redirect("/")
